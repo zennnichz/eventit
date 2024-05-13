55bbfbd1671b519dbcbee16883fbb489473fb22f
Vladislav <vladzenevich03@mail.ru>
2024-05-13 16:40:14 +0300

    upd1

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-    // publicRoutes:[
-    //     '/',
-    //     'events/;id',
-    //     '/api/webhook/clerk',
-    //     '/api/webhook/stripe',
-    //     '/api/uploadthing'
-    // ],
-    // ignoredRoutes:[
-    //     '/api/webhook/clerk',
-    //     '/api/webhook/stripe',
-    //     '/api/uploadthing'
-    // ]
+export default authMiddleware({
+    publicRoutes: [
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+    ],
+    ignoredRoutes: [
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+    ]
 });
 
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
